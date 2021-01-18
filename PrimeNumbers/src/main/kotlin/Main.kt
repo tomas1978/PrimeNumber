19d242b31f8d27746fc141717cdff5e1226cc94d
tomas1978 <tomas.nilsson78@gmail.com>
2021-01-18 07:31:28 +0100

    Added function for primes in an interval

--- PrimeNumbers/src/main/kotlin/Main.kt
+++ PrimeNumbers/src/main/kotlin/Main.kt
@@ -1,13 +1,28 @@
+import kotlin.math.sqrt
+
 fun isPrime(testNumber:Int):Boolean {
+    if(testNumber<2)
+        return false
     var prime:Boolean=true
-    for(i in 2..Math.sqrt(testNumber.toDouble()).toInt()) {
+    for(i in 2..sqrt(testNumber.toDouble()).toInt()) {
         if(testNumber%i==0)
             return false
     }
     return true
 }
 
+fun writePrimesInInterval(lower:Int, higher:Int) {
+    for(i in lower..higher) {
+        if(isPrime(i)) {
+            println(i)
+        }
+    }
+}
+
 fun main() {
     var number:Int=71
-    println(isPrime(number))
+    println("The program writes all primes within an interval to the screen")
+    var lower:Int = 100
+    var upper:Int = 200
+    println(writePrimesInInterval(2,100))
 }
